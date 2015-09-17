dcc96513250bb2bdbddbe750efdca9f62def8303
Steven Cox <scox@stars.renci.org>
2015-09-17 13:44:07 -0400

    workers

--- bin/fabfile.py
+++ bin/fabfile.py
@@ -13,8 +13,9 @@ from fabric.contrib.files import exists
 # http://www.itzgeek.com/how-tos/linux/centos-how-tos/monitor-and-manage-services-with-monit-on-centos-7-rhel-7.html
 
 # the user to use for the remote commands
-#env.user = 'evryscope'
+
 env.user = 'scox'
+env.user = 'evryscope'
 
 root  = "/projects/stars"
 stack = "%s/stack" % root
@@ -41,15 +42,22 @@ base_apps = {
 }
 
 # Servers where the commands are executed
-headnode_hosts = (
+head_nodes = (
     'stars-c0.edc.renci.org',
     'stars-c1.edc.renci.org'
 )
-worker_hosts = ()
+worker_nodes = (
+    'stars-c2.edc.renci.org',
+    'stars-c3.edc.renci.org',
+    'stars-c4.edc.renci.org',
+    'stars-c5.edc.renci.org',
+    'stars-c6.edc.renci.org',
+    'stars-c7.edc.renci.org'
+)
 
-zookeeper_hosts=( 'stars-c0.edc.renci.org' )
+zookeeper_nodes=( 'stars-c0.edc.renci.org' )
 
-cluster_nodes = tuple(list(headnode_hosts) + list(worker_hosts))
+cluster_nodes = tuple(list(head_nodes) + list(worker_nodes))
 
 ##################################################################
 ##
@@ -60,10 +68,13 @@ cluster_nodes = tuple(list(headnode_hosts) + list(worker_hosts))
 def stackpath (name):
     return "%s/%s" % (stack, name)
 def execute_op (mode, install, clean):
+    installed = True
     if mode == "install":
         install ()
     elif mode == "clean":
         clean ()
+        installed = False
+    return installed
 
 # Tar
 
@@ -84,7 +95,7 @@ def deploy_uri_tar (mode, name):
             untar_and_link (name, os.path.basename (uri))
     def clean ():
         run ('rm -rf %s' % path)
-    execute_op (mode, install, clean)
+    return execute_op (mode, install, clean)
 
 def deploy_uri_rpm (mode, name):
     path = "%s/%s" % (stack, name)
@@ -97,7 +108,7 @@ def deploy_uri_rpm (mode, name):
                 yum_install (mode, name, dist_map[name])
     def clean ():
         yum_install (mode, name, name)
-    execute_op (mode, install, clean)
+    return execute_op (mode, install, clean)
 
 def deploy_tar (mode, name):
     path = "%s/%s" % (stack, name)
@@ -107,7 +118,7 @@ def deploy_tar (mode, name):
             untar_and_link (name, dist_map [name])
     def clean ():
         run ('rm -rf %s' % path)
-    execute_op (mode, install, clean)
+    return execute_op (mode, install, clean)
 
 # Yum
 
@@ -115,9 +126,11 @@ def install_mesosphere_repo ():
     mesosphere_repo='http://repos.mesosphere.com/el/7/noarch/RPMS/mesosphere-el-repo-7-1.noarch.rpm'
     sudo ('if [ -z "$( rpm -qa | grep mesosphere-el-repo )" ]; then yum install --assumeyes --quiet %s; fi' % mesosphere_repo)
 
-def get_yum_command (sudo=False, install=True):
-    command = Template('if [ "$$(rpm -qa | grep -c %s)" $compareop 0  ]; then $yum $command --assumeyes --quiet %s; fi')
+def yum_installed (package):
+    return 'if [ "$(rpm -qa | grep -c %s)" -gt 0  ]; then true; fi' % package
 
+def get_yum_command (sudo=True, install=True):
+    command = Template('if [ "$$(rpm -qa | grep -c %s)" $compareop 0  ]; then $yum $command --assumeyes --quiet %s; fi')
     yum = "sudo yum" if sudo else "yum"
     if install:
         command = command.substitute (yum=yum, compareop='==', command='install')
@@ -131,7 +144,7 @@ def yum_install (mode, query, package):
         sudo (get_yum_command () % (query, package))
     def clean ():
         sudo (get_yum_command(sudo=False, install=False) % (query, package))
-    execute_op (mode, install, clean)
+    return execute_op (mode, install, clean)
 
 # SystemD
 
@@ -144,7 +157,7 @@ def configure_service (mode, service):
     def clean ():
         sudo ('systemctl stop %s' % service)
         sudo ('systemctl disable %s' % service)
-    execute_op (mode, install, clean)
+    return execute_op (mode, install, clean)
 
 ##################################################################
 ##
@@ -165,7 +178,7 @@ def web (mode="install"):
         local ('sudo rm -rf /etc/nginx')
         for app in base_apps:
             local (get_yum_command (sudo=True, install=False) % (app, base_apps[app]))
-    execute_op (mode, install, clean)
+    return execute_op (mode, install, clean)
 
 def proxy (mode="install"):
     def install ():        
@@ -177,9 +190,9 @@ def proxy (mode="install"):
     def clean ():
         local ('sudo cp /etc/nginx/nginx.conf.orig /etc/nginx/nginx.conf')
         local ('sudo cp -r /etc/nginx/nginx.conf.orig /etc/nginx/conf.d')
-    execute_op (mode, install, clean)
+    return execute_op (mode, install, clean)
         
-@hosts(headnode_hosts)
+@hosts(head_nodes)
 def heads (mode="install"):
     install_mesosphere_repo ()
     package_map = {
@@ -196,34 +209,63 @@ def heads (mode="install"):
     def install ():
         sudo ('echo 2 > /etc/mesos-master/quorum')
         sudo ('echo 1 > /var/lib/zookeeper/myid')
-        addr = local ("ping -c 1 %s | awk 'NR==1{gsub(/\(|\)/,\"\",$3);print $3}'" % zookeeper_hosts, capture=True)
+        addr = local ("ping -c 1 %s | awk 'NR==1{gsub(/\(|\)/,\"\",$3);print $3}'" % zookeeper_nodes, capture=True)
         zoo_cfg = """
 server1.%s:2888:3888        
 dataDir=/var/log/zookeeper
 clientPort=2181
 """ % addr
         sudo ('echo \"%s\" > /etc/zookeeper/conf/zoo.cfg' % zoo_cfg)
-
-        sudo ('cp %s/iptables.headnode /etc/sysconfig/iptables' % conf)
-        sudo ('service iptables restart')
-        sudo ('service iptables status')
+        conf_base_node (mode)
     def clean ():
         sudo ('rm -rf /etc/mesos-master/quorum')
         sudo ('rm -rf /var/lib/zookeeper/myid')
         sudo ('rm -rf /etc/zookeeper/conf/zoo.cfg')
-    execute_op (mode, install, clean)
 
-@hosts(headnode_hosts)
-def services (mode="install"):
     configure_service (mode, 'mesos-master')
     configure_service (mode, 'marathon')
     configure_service (mode, 'chronos')
 
-@hosts(cluster_nodes)
-def all (mode="install"):
+    return execute_op (mode, install, clean)
+
+@hosts(head_nodes)
+def head_iptables (mode="install"):
     def install ():
-        addr = socket.gethostbyname(zookeeper_hosts(0))
-        print "zookeeper host: %s" % addr
+        sudo ('if [ ! -f /etc/sysconfig/iptables.orig ]; then cp /etc/sysconfig/iptables /etc/sysconfig/iptables.orig; fi')
+        sudo ('cp %s/iptables.headnode /etc/sysconfig/iptables' % conf)
+        sudo ('service iptables restart')
+        sudo ('service iptables status')
+    def clean ():
+        sudo ('cp /etc/sysconfig/iptables.orig /etc/sysconfig/iptables')
+        sudo ('service iptables stop')
+        sudo ('service iptables status')
+
+''' Applies to every worker node. Configure worker and firewall. '''
+@hosts(worker_nodes)
+def workers (mode="install"):
+    def install ():
+        install_mesosphere_repo ()
+        yum_install (mode, "mesos-", "mesos")
+        conf_base_node (mode)
+        sudo ("systemctl enable mesos-slave")
+        sudo ("service mesos-slave restart")
+        sudo ("service mesos-slave status")
+        sudo ('service iptables stop')
+    def clean ():
+        sudo ('service iptables start')
+        sudo ('service iptables status')
+        sudo ("systemctl disable mesos-slave")
+        sudo ("service mesos-slave stop")
+        yum_install (mode, "mesos-", "mesos")
+        conf_base_node (mode)
+    return execute_op (mode, install, clean)
+
+''' Applies to every machine '''
+@hosts(cluster_nodes)
+def conf_base_node (mode="install"):
+    def install ():
+        addr = local ("ping -c 1 %s | awk 'NR==1{gsub(/\(|\)/,\"\",$3);print $3}'" % zookeeper_nodes, capture=True)
+        print "zookeeper hosts: %s" % addr
         text = "%s:2181" % addr
         sudo ('echo zk://%s/mesos > /etc/mesos/zk' % text)
         for app in base_apps:
@@ -232,12 +274,15 @@ def all (mode="install"):
         sudo ('rm -rf /etc/mesos/zk')
         for app in base_apps:
             yum_install (mode, app, base_apps[app])
-        
-@hosts(zookeeper_hosts)
+    return execute_op (mode, install, clean)
+
+''' Configure zookeeper cluster '''        
+@hosts(zookeeper_nodes)
 def zookeeper (mode="install"):
     configure_service (mode, 'zookeeper')
-        
-@hosts(zookeeper_hosts)
+
+''' Configure general tools underlying the cluster '''
+@hosts(zookeeper_nodes)
 def devstack (mode="install"):
     run('hostname -f')
     run('mkdir -p %s' % dist)
@@ -256,6 +301,7 @@ def devstack (mode="install"):
         deploy_uri_tar (mode, 'hadoop')
         deploy_uri_tar (mode, 'tachyon')
 
+''' Configure astroinformatics stack '''
 @hosts(cluster_nodes)
 def astro (mode="install"):
     deploy_uri_rpm (mode, 'swarp')
