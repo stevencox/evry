012c434df401e8163fa96162d6975b3fac4296d5
StarsCluster <scox@renci.org>
2015-10-01 13:41:37 -0400

    db auto

--- bin/fabfile.py
+++ bin/fabfile.py
@@ -15,6 +15,7 @@
 
 import os
 import socket
+import StringIO
 
 from string import Template
 
@@ -24,6 +25,7 @@ from fabric.api import execute
 from fabric.api import hosts
 from fabric.api import local
 from fabric.api import parallel
+from fabric.api import put
 from fabric.api import settings
 from fabric.api import run
 from fabric.api import sudo
@@ -207,6 +209,11 @@ def yum_install (mode, query, package):
         sudo (get_yum_command(install=False) % (query, package))
     return execute_op (mode, install, clean)
 
+def yum_install_all (mode, packages):
+    packages = packages.split (' ')
+    for p in packages:
+        yum_install (mode, p, p)
+
 # SystemD
 
 ''' Configure a systemd service ''' 
@@ -310,9 +317,22 @@ def chronos (mode="install"):
         yum_install (mode, "chronos-", "chronos")
     return execute_op (mode, install, clean)
 
+def generate_config (template, context, output=None, use_sudo=False):
+    result = None
+    with open (template) as stream:
+        template_obj = Template (stream.read ())
+        result = template_obj.safe_substitute (context)
+    if output:
+        put (StringIO.StringIO (result), output, use_sudo=use_sudo, temp_dir='/tmp')
+    return result
+
+@hosts(head_nodes)
+def mesos (mode="install"):
+    mesos_21 (mode)
+
 @parallel
 @hosts(head_nodes)
-def mesos(mode="install"):
+def mesos_24(mode="install"):
     def install ():
         yum_install (mode, "mesos-", "mesos")
         sudo ('rm -rf /etc/mesos-master/quorum.rpm*')
@@ -320,6 +340,56 @@ def mesos(mode="install"):
         sudo ('chown -R %s /var/log/mesos' % env.user)
         sudo ('chown -R %s /var/lib/mesos' % env.user)
         sudo ('cp %s/mesos/mesos-master.service /usr/lib/systemd/system/' % conf)
+        configure_service (mode, 'mesos-master')
+    def clean ():
+        configure_service (mode, 'mesos-master')
+        sudo ('rm -rf /etc/mesos-master/quorum')
+        sudo ('rm -f /usr/lib/systemd/system/mesos-master.service')
+        yum_install (mode, "mesos-", "mesos")
+    return execute_op (mode, install, clean)
+
+def mesos_21_install (mode):
+    def install ():
+        sudo ('rm -rf /usr/lib/systemd/system/mesos-master.service')
+        with settings(warn_only=True):
+            libs = ' '.join ([ 'apache-maven python-devel java-1.7.0-openjdk-devel zlib-devel libcurl-devel',
+                               'openssl-devel cyrus-sasl-devel cyrus-sasl-md5 apr-devel subversion-devel apr-util-devel' ])
+            yum_install_all (mode, libs) 
+        with cd (opt):
+            sudo ('rm -rf mesos')
+            sudo ('mkdir -p mesos')
+            sudo ('chown %s mesos' % env.user)
+            run ('mkdir -p mesos')
+            with cd ('mesos'):
+                run ('tar xzf %s/mesos-renci-0.21.0.tar.gz' % dist)
+                with cd ('mesos-0.21.0'):
+                    sudo ('make install')
+    def clean ():
+        sudo ('rm -rf %/mesos' % opt)
+    return execute_op (mode, install, clean)
+
+@hosts(head_nodes)
+def mesos_21(mode="install"):
+    def install ():
+        mesos_21_install (mode)
+        ip_addr = run ('hostname -I')
+        zk_string = run ('cat /etc/mesos/zk')
+        generate_config (
+            template='%s/mesos/mesos-master.service.custom' % conf,
+            context={
+                'EXE'      : '%s/mesos/mesos-0.21.0/bin/mesos-master.sh' % opt,
+                'IP'       : ip_addr,
+                'ZK'       : zk_string,
+                'LOGLEVEL' : 'WARNING',
+                'LOGDIR'   : '/var/log/mesos',
+                'QUORUM'   : 2
+            },
+            output='/usr/lib/systemd/system/mesos-master.service',
+            use_sudo=True)
+        sudo ('rm -rf /etc/mesos-master/quorum.rpm*')
+        sudo ('echo 2 > /etc/mesos-master/quorum')
+        sudo ('chown -R %s /var/log/mesos' % env.user)
+        sudo ('chown -R %s /var/lib/mesos' % env.user)
         configure_service (mode, 'mesos-master')
     def clean ():
         configure_service (mode, 'mesos-master')
@@ -382,23 +452,30 @@ def firewall (mode="install"):
     def install ():
         sudo ('if [ ! -f /etc/sysconfig/iptables.orig ]; then cp /etc/sysconfig/iptables /etc/sysconfig/iptables.orig; fi')
         sudo ('cp %s/iptables.headnode /etc/sysconfig/iptables' % conf)
-        sudo ('service iptables restart')
-        sudo ('service iptables status')
+#        sudo ('service iptables restart')
+        sudo ('service iptables stop') # spark assigns random ports for driver/worker communication.
+#        sudo ('service iptables status')
     def clean ():
         sudo ('cp /etc/sysconfig/iptables.orig /etc/sysconfig/iptables')
         sudo ('service iptables stop')
         sudo ('service iptables status')
     return execute_op (mode, install, clean)
 
-''' Applies to every worker node. Configure worker and firewall. '''
 @parallel
 @hosts(worker_nodes)
 def work (mode="install"):
+    work_21 (mode)
+
+''' Applies to every worker node. Configure worker and firewall. '''
+@parallel
+@hosts(worker_nodes)
+def work_24 (mode="install"):
     mesosphere_repo (mode)
     # Needed by evry app
     yum_install (mode, 'postgresql-devel', 'postgresql-devel')
     base (mode)
     def install ():
+        yum_install (mode, "mesos-", "mesos")
         sudo ("systemctl enable mesos-slave")
         sudo ("service mesos-slave restart")
         sudo ("service mesos-slave status")
@@ -412,8 +489,43 @@ def work (mode="install"):
             sudo ('service iptables status') 
             sudo ("systemctl disable mesos-slave")
             sudo ("service mesos-slave stop")
-    yum_install (mode, "mesos-", "mesos")
+            yum_install (mode, "mesos-", "mesos")
     return execute_op (mode, install, clean)
+
+@parallel
+@hosts(worker_nodes)
+def work_21 (mode="install"):
+    mesos_21_install (mode)
+    mesosphere_repo (mode)
+    # Needed by evry app
+    yum_install (mode, 'postgresql-devel', 'postgresql-devel')
+    base (mode)
+    def install ():
+        zk_string = run ('cat /etc/mesos/zk')
+        generate_config (
+            template='%s/mesos/mesos-slave.service.custom' % conf,
+            context={
+                'EXE'      : '%s/mesos/mesos-0.21.0/bin/mesos-slave.sh' % opt,
+                'ZK'       : zk_string,
+                'LOGDIR'   : '/var/log/mesos'
+            },
+            output='/usr/lib/systemd/system/mesos-slave.service',
+            use_sudo=True)
+        sudo ("systemctl enable mesos-slave")
+        sudo ("service mesos-slave restart")
+        sudo ("service mesos-slave status")
+        sudo ('service iptables stop')
+        # Support application installation
+        sudo ('mkdir -p %s' % opt)
+        sudo ('chown %s %s' % (env.user, opt))
+    def clean ():
+        with settings(warn_only=True):
+            sudo ('service iptables start')
+            sudo ('service iptables status') 
+            sudo ("systemctl disable mesos-slave")
+            sudo ("service mesos-slave stop")
+            yum_install (mode, "mesos-", "mesos")
+    return execute_op (mode, install, clean)    
 
 ''' Applies to every machine '''
 @parallel
@@ -469,19 +581,30 @@ def core (mode="install"):
                      --header "Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com%2F; oraclelicense=accept-securebackup-cookie" \
                      "http://download.oracle.com/otn-pub/java/jdk/8u60-b27/jdk-8u60-linux-x64.tar.gz" """)
     with cd (stack):
-        deploy_tar     (mode, 'jdk')
-        deploy_uri_tar (mode, 'maven')
-        deploy_uri_tar (mode, 'scala')
         spark (mode)
+        maven (mode)
+        deploy_tar     (mode, 'jdk')
+        deploy_uri_tar (mode, 'scala')
         deploy_uri_tar (mode, 'node')
         deploy_uri_tar (mode, 'mongodb')
         deploy_uri_tar (mode, 'hadoop')
         deploy_uri_tar (mode, 'tachyon')
 
 @hosts(zookeeper_nodes)
+def maven (mode="install"):
+    deploy_uri_tar (mode, 'maven')
+    run ('cp %s/maven/settings.xml %s/maven/current/conf' % (conf, stack))
+
+@hosts(zookeeper_nodes)
 def spark (mode="install"):
     deploy_uri_tar (mode, 'spark')
-    run ('cp %s/spark/spark-env.sh %s/spark/current/conf' % (conf, stack))
+    generate_config (
+        template='%s/spark/spark-env.sh' % conf,
+        context={
+            'STARS_CONF' : conf,
+            'STARS_DIST' : dist
+        },
+        output='%s/spark/current/conf/spark-env.sh' % stack)
 
 @hosts(db_nodes)
 def db (mode="install"):
@@ -510,9 +633,9 @@ def db (mode="install"):
         configure_service (mode, 'postgresql')
 
         sudo ('su - postgres -c "psql < %s/evry/db/evryscope.init.sql"' % app)
-        sudo ('su - postgres -c "psql evryscope < %s/evry/db/evryscope.schema"' % app)
-        sudo ("su - postgres -c \"psql evryscope -c '\\d light_curves' \" ")
-        sudo ("su - postgres -c \"psql evryscope -c '\\d images' \" ")
+        sudo ('su - postgres -c "psql %s < %s/evry/db/evryscope.schema"' % (env.user, app))
+        sudo ("su - postgres -c \"psql %s -c '\\d light_curves' \" " % env.user)
+        sudo ("su - postgres -c \"psql %s -c '\\d images' \" " % env.user)
 
     def clean ():
         configure_service (mode, 'postgresql')
--- conf/spark/spark-env.sh
+++ conf/spark/spark-env.sh
@@ -1,4 +1,6 @@
-#!/usr/bin/env bash
+0#!/usr/bin/bash
+
+# #!/usr/bin/env bash
 
 SPARK_LOCAL_IP=$(hostname -I | sed -e "s, ,,g")
 export LIBPROCESS_IP=${SPARK_LOCAL_IP}
@@ -8,8 +10,10 @@ export MESOS_MASTER=mesos://${SPARK_LOCAL_IP}:5050
 
 export MASTER=$MESOS_MASTER
 #export SPARK_EXECUTOR_URI=hdfs://mesos-prod.isilon-1.oscar.priv:8020/user/cluster/spark/spark-1.4.1-bin-hadoop2.4.tgz
-export SPARK_EXECUTOR_URI=/projects/stars/dist/spark-1.5.0-bin-hadoop2.6.tgz
+export SPARK_EXECUTOR_URI=${STARS_DIST}/spark-1.5.0-bin-hadoop2.6.tgz
 
 export SPARK_DAEMON_JAVA_OPTS+=""
 
-source /projects/stars/venv/bin/activate
+echo "Initializing Spark Environment"
+source ${STARS_CONF}/env.sh
+
